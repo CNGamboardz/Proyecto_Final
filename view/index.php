e2792a8f853c1f75a51c458c60aab10795c25f88
CNGamboardz <gyt221041@gmail.com>
2024-10-11 20:26:15 -0600

    Se ha creado el css

--- view/index.php
+++ view/index.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../view/css/style.css">
+    
     <title>Chiapas Oculto</title>
 </head>
 <body>
