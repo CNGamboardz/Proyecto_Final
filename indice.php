df5ed382c762a89e84e8679d0c299269af812c02
BrendaP-18 <brenda.palasio62@unach.mx>
2024-10-19 17:57:27 -0600

    Se agrego los paquetes y tours del menu principal

--- indice.php
+++ indice.php
@@ -18,6 +18,39 @@
 
     <br><br>
     <img src="./view/img/banner.png" width="100%" height="100%">
-    
+    <br>
+    <br>
+    <h1>Nuestro objetivo</h1>
+    <p class="parrafo">
+        Nuestro objetivo es posicionar a Chiapas como un destino líder en turismo alternativo, ofreciendo experiencias únicas 
+        que conecten a los viajeros con la riqueza natural, cultural y espiritual de la región. A través de nuestra plataforma 
+        Chiapas Oculto, facilitaremos el acceso a rutas de lugares y actividades exclusivas, promoviendo un turismo sostenible 
+        y responsable que beneficie tanto a las comunidades locales como al medio ambiente.
+    </p>
+
+    <section class="tours">
+        <br>
+        <br>
+        <h1>Los mejores Tours por Chiapas</h1>
+        <br>
+        <table width="100%" class="tabla_tours">
+            <tr> 
+                    <td><img src="view/img/p1.jpg" alt="" idth="400" height="370"></td>
+                    <td><img src="view/img/p2.jpg" alt="" idth="400" height="370"></td>
+                    <td><img src="view/img/p3.jpg" alt="" idth="400" height="370"></td>
+            </tr>
+        </table>
+        <br>
+        <br>
+        <h1>Los mejores Paquetes por Chiapas</h1>
+        <br>
+        <table width="100%" class="tabla_paquetes">
+            <tr> 
+                    <td><img src="view/img/20.jpg" alt="" idth="400" height="370"></td>
+                    <td><img src="view/img/19.jpg" alt="" idth="400" height="370"></td>
+                    <td><img src="view/img/13.jpg" alt="" idth="400" height="370"></td>
+            </tr>
+        </table>
+    </section>
 </body>
 </html>
