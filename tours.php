594eac3978311f20c9d6ba61ca63a4f94d3d5d19
CNGamboardz <gyt221041@gmail.com>
2024-10-19 16:42:29 -0600

    se ah actualizado el menú

--- tours.php
+++ tours.php
@@ -6,16 +6,31 @@
     <title>Chiapas Oculto</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="view/css/style.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     
     <title>Tours Chiapas</title>
+
+    <style>
+
+    </style>
+    
 </head>
 <body>
     <?php require_once("./view/layout/header.php");?>
     <br><br>
 
     <img src="./view/img/tour.png" width="100%" height="100%">
-    
+
+    <br><br>
+
+    <div class="container">
+        <p class="text-border">En estos tours, disfrutarás de una experiencia inolvidable con la agencia Chiapas Oculto. 
+            Te ofrecemos aventuras únicas llenas de emoción, exploración y momentos inolvidables. 
+            ¡Prepárate para descubrir lo mejor del destino con nosotros!
+
+        </p>
+    </div>
+
 </body>
 </html>
