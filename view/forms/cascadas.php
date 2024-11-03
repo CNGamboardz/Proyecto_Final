7676ba9104cf1775d2f945047c0f237aaca2e9ea
Lcho640 <horacio.moreno34@unach.mx>
2024-11-03 15:53:01 -0600

    se agrego completamente el primer itinerario de cascadas de agua azul

--- view/forms/cascadas.php
+++ view/forms/cascadas.php
@@ -12,6 +12,24 @@
     <title>Cascadas de Agua Azul</title>
 </head>
 
+<script>
+    let count = 2;
+
+    function increase() {
+        if (count < 10) {
+            count++;
+            document.getElementById('counter').innerText = count;
+        }
+    }
+
+    function decrease() {
+        if (count > 2) {
+            count--;
+            document.getElementById('counter').innerText = count;
+        }
+    }
+</script>
+
 <body>
     <?php require_once("./view/layout/header.php"); ?>
     <br><br>
@@ -21,11 +39,128 @@
 
     <div class="table-responsive">
         <table width="100%" class="tabla_tours">
-            <tr> 
-               <td><img src="view/img/Cascadas.jpg" alt="" class="img-fluid" width="845px" height="525px"></td>
-               <td></td>
+            <tr>
+                <td>
+                    <img src="view/img/Cascadas.jpg" alt="" class="img-fluid" width="845px" height="525px">
+                </td>
+                <td>
+                    <div class="recuadro">
+                        <div style="margin-left: 40px;">
+                            <h6>Inicio/Tour por Chiapas</h6>
+                            <h2>Cascadas de Agua Azul</h2>
+                            <h5>$2,500.00</h5>
+                            <h5>Por persona <br> Min. 2 personas <br> Max. 10 personas</h5>
+                            <h5>Duracion: 1 dia</h5>
+                            <br>
+                            <h5>Fecha a reservar</h5>
+                        </div>
+                        <div class="counter-container">
+                            <button class="counter-button" onclick="decrease()">−</button>
+                            <span class="counter-value" id="counter">2</span>
+                            <button class="counter-button" onclick="increase()">+</button>
+                            <button class="btn1">
+                                <p style="color: white;">AÑADIR AL CARRITO</p>
+                            </button>
+                        </div>
+                        <br>
+                        <center>
+                            <button class="btn2">
+                                <img src="view/img/logo_whats.png" width="30px"> Resolver tus dudas
+                            </button>
+                            <br>
+                            <img src="view/img/visa-mastercard.png" width="150px">
+                            <img src="view/img/mercadopago.png" width="150px">
+                        </center>
+                    </div>
+                </td>
             </tr>
         </table>
+    </div>
+
+    <div class="tabla_contenido">
+        <table width="90%">
+            <tr>
+                <td>
+                    <p class="letras">Descipción</p>
+                    <div class="textos">
+                        <img src="view/img/icono-mapa.png" width="70px"> Ubicación de salida: Tuxtla Gutiérrez Chiapas
+                        <br>
+                        <img src="view/img/icono-reloj.png" width="70px"> Hora de salida: 7:30 am
+                    </div>
+                    <div class="itinerario">
+                        <p class="letras">Itinerario</p>
+                        <p>
+                            <b>07:30 AM</b> - Recepción en el hotel de Tuxtla Gutiérrez
+                            El conductor experimentado te recogerá en tu hotel. Asegúrate de estar listo para salir a tiempo.
+                        </p>
+                        <p>
+                            <b>08:00 AM</b> - Salida hacia las Cascadas de Agua Azul
+                            Comienza el viaje en transporte turístico compartido. Durante el trayecto, podrás disfrutar del hermoso paisaje chiapaneco.
+                        </p>
+
+                        <p>
+                            <b>09:00 AM</b> - Llegada a las Cascadas de Agua Azul
+                            Al llegar, recibirás una breve introducción sobre el lugar y tendrás tiempo para tomar algunas fotos de las impresionantes vistas.
+                        </p>
+
+                        <p>
+                            <b>09:30 AM</b> - Tiempo libre para disfrutar de las Cascadas
+                            Explora a tu ritmo. Camina por los senderos, admira las cascadas y, si lo deseas, refréscate en las pozas de agua cristalina.
+                        </p>
+
+                        <p>
+                            <b>11:30 AM</b> - Tiempo libre para el almuerzo (no incluido)
+                            Aprovecha este momento para descansar y disfrutar de la comida. Puedes llevar un almuerzo empacado o comprar algo en los restaurantes locales.
+                        </p>
+
+                        <p>
+                            <b>12:30 PM</b> - Continuación de la visita a las Cascadas
+                            Sigue explorando las diferentes secciones de las cascadas. Cada rincón ofrece una vista espectacular, así que no olvides tu cámara.
+                        </p>
+
+                        <p>
+                            <b>02:30 PM</b> - Salida hacia Tuxtla Gutiérrez
+                            Reúnete con el grupo y comienza el regreso al hotel.
+                        </p>
+
+                        <p>
+                            <b>04:30 PM</b> - Llegada a Tuxtla Gutiérrez
+                            Finaliza el recorrido. El conductor te dejará en tu hotel, donde podrás descansar después de un día lleno de aventuras.
+                        </p>
+                    </div>
+
+                </td>
+                <td>
+                    <div class="recuadro2">
+                        <center>
+                            <h3 style="color: #2AB7CA;">Incluye</h3>
+                        </center>
+                        <img src="view/img/flecha_verde.png" width="20px"> Transportación turística en servicio grupal o compartido
+                        <br>
+                        <img src="view/img/flecha_verde.png" width="20px"> Recepción en hotel de Tuxtla Gutiérrez
+                        <br>
+                        <img src="view/img/flecha_verde.png" width="20px"> Entradas a todos los sitios turísticos
+                        <br>
+                        <img src="view/img/flecha_verde.png" width="20px"> Seguro de viajero a bordo de la unidad.
+                        <br>
+                        <img src="view/img/flecha_verde.png" width="20px"> Conductor experimentado
+                    </div>
+                    <br>
+                    <div class="recuadro3">
+                        <center>
+                            <h3 style="color: #2AB7CA;">No incluye</h3>
+                        </center>
+                        <img src="view/img/incono-equis.png" width="20px"> Alimentos
+                        <br>
+                        <img src="view/img/incono-equis.png" width="20px"> Servicios no especificados
+                        <br>
+                        <img src="view/img/incono-equis.png" width="20px"> Guías en zona arqueológica (pago no obligatorio)
+                    </div>
+                </td>
+            </tr>
+
+        </table>
+    </div>
 
     <br><br><br><br>
     <table class="piepagina" width=100%>
