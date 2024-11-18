d9eb67cfb2605c908e0328ca80e2653b25149db7
CNGamboardz <gyt221041@gmail.com>
2024-11-18 11:54:02 -0600

    se ha creado botones para los administradores y editar.php

--- view/forms/tours.php
+++ view/forms/tours.php
@@ -47,6 +47,16 @@
                     <br>
                     <p class="pequeno"><b>$<?php echo number_format($paquete['Precio'], 2); ?></b></p>
                 </a>
+                
+                <?php 
+                // Verificar si el usuario está logueado y tiene id_rango == 1
+                if (isset($_SESSION['id_rango']) && $_SESSION['id_rango'] == 1): ?>
+                    <div class="acciones">
+                        <a href="editar_paquete.php?id=<?php echo $paquete['id_catalogo']; ?>" class="btn btn-warning"><b>EDITAR</b></a>
+                        <br>
+                        <a href="eliminar_paquete.php?id=<?php echo $paquete['id_catalogo']; ?>" class="btn btn-danger"><b>ELIMINAR</b></a>
+                    </div>
+                <?php endif; ?>
             </td>
             <?php 
             $counter++; 
@@ -65,40 +75,40 @@
     <br><br><br><br><br><br><br>
 
     <table class="piepagina" width="100%">
-            <tr>
-                <td width="15%">
-                    <br><br><br>
-                    <img src="./view/img/logo_chiapasoculto.png" class="img-fluid" alt="Logo Chiapas Oculto">
-                    <br><br>
-                </td>
-                <td width="22%">
-                    <center><h5>Servicio al Cliente</h5></center>
-                    <a href="index.php?u=contacto"><ul class="custom-bullets"><li>Contacto</li></ul></a>
-                    <a href="index.php?u=login"><ul class="custom-bullets"><li>Mi Cuenta</li></ul></a>
-                </td>
-                <td width="22%">
-                    <center><h5>Tours Destacados</h5></center>
-                    <a href="index.php?u=arcotete"><ul class="custom-bullets2"><li>El Arcote</li></ul></a>
-                    <a href="index.php?u=lagunasmontebello"><ul class="custom-bullets2"><li>Lagunas de Montebello</li></ul></a>
-                    <a href="index.php?u=palenque"><ul class="custom-bullets2"><li>Zona Arqueológica De Palenque</li></ul></a>
-                    <a href="index.php?u=zonarqueologica"><ul class="custom-bullets2"><li>Zona Arqueológica De Tonina</li></ul></a>
-                </td>
-                <td width="22%">
-                    <center><h5>Paquetes Destacados</h5></center>
-                    <a href="index.php?u=arqueologiaselva"><ul class="custom-bullets2"><li>Arqueología y Selva</li></ul></a>
-                    <a href="index.php?u=paraisoselva"><ul class="custom-bullets2"><li>Paraíso en la Selva</li></ul></a>
-                    <a href="index.php?u=expedicionchiapaneca"><ul class="custom-bullets2"><li>Expedición Chiapaneca</li></ul></a>
-                    <a href="index.php?u=rutasmchiapas"><ul class="custom-bullets2"><li>Rutas Mágicas de Chiapas</li></ul></a>
-                </td>
-                <td width="22%">
-                    <br><br><br>
-                    <a class="normal" href="https://www.google.com/maps/place/Salon+Gema/@16.7725603,-93.0872266,463m/data=!3m1!1e3!4m6!3m5!1s0x85ed275d3b624c83:0x4d25f42cb24d00ed!8m2!3d16.77325!4d-93.0885407!16s%2Fg%2F11gh0511bn?authuser=0&entry=ttu&g_ep=EgoyMDI0MTExMy4xIKXMDSoASAFQAw%3D%3D">
-                        <img src="./view/img/geolocalizacion.png" width="20%" alt="Icono de geolocalización">
-                        Ver Ubicación
-                    </a>
-                </td>
-            </tr>
-        </table>
+        <tr>
+            <td width="15%">
+                <br><br><br>
+                <img src="./view/img/logo_chiapasoculto.png" class="img-fluid" alt="Logo Chiapas Oculto">
+                <br><br>
+            </td>
+            <td width="22%">
+                <center><h5>Servicio al Cliente</h5></center>
+                <a href="index.php?u=contacto"><ul class="custom-bullets"><li>Contacto</li></ul></a>
+                <a href="index.php?u=login"><ul class="custom-bullets"><li>Mi Cuenta</li></ul></a>
+            </td>
+            <td width="22%">
+                <center><h5>Tours Destacados</h5></center>
+                <a href="index.php?u=arcotete"><ul class="custom-bullets2"><li>El Arcote</li></ul></a>
+                <a href="index.php?u=lagunasmontebello"><ul class="custom-bullets2"><li>Lagunas de Montebello</li></ul></a>
+                <a href="index.php?u=palenque"><ul class="custom-bullets2"><li>Zona Arqueológica De Palenque</li></ul></a>
+                <a href="index.php?u=zonarqueologica"><ul class="custom-bullets2"><li>Zona Arqueológica De Tonina</li></ul></a>
+            </td>
+            <td width="22%">
+                <center><h5>Paquetes Destacados</h5></center>
+                <a href="index.php?u=arqueologiaselva"><ul class="custom-bullets2"><li>Arqueología y Selva</li></ul></a>
+                <a href="index.php?u=paraisoselva"><ul class="custom-bullets2"><li>Paraíso en la Selva</li></ul></a>
+                <a href="index.php?u=expedicionchiapaneca"><ul class="custom-bullets2"><li>Expedición Chiapaneca</li></ul></a>
+                <a href="index.php?u=rutasmchiapas"><ul class="custom-bullets2"><li>Rutas Mágicas de Chiapas</li></ul></a>
+            </td>
+            <td width="22%">
+                <br><br><br>
+                <a class="normal" href="https://www.google.com/maps/place/Salon+Gema/@16.7725603,-93.0872266,463m/data=!3m1!1e3!4m6!3m5!1s0x85ed275d3b624c83:0x4d25f42cb24d00ed!8m2!3d16.77325!4d-93.0885407!16s%2Fg%2F11gh0511bn?authuser=0&entry=ttu&g_ep=EgoyMDI0MTExMy4xIKXMDSoASAFQAw%3D%3D">
+                    <img src="./view/img/geolocalizacion.png" width="20%" alt="Icono de geolocalización">
+                    Ver Ubicación
+                </a>
+            </td>
+        </tr>
+    </table>
 
     <br><br>
     <?php require_once("./view/layout/footer.php");?>
