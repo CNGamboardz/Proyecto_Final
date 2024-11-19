125e598eb38a71f41895d35108bb97b5f62aa3e6
CNGamboardz <gyt221041@gmail.com>
2024-11-18 20:09:02 -0600

    se han subido en todos los formularios los iconos del logo

--- view/forms/paraisoselva.php
+++ view/forms/paraisoselva.php
@@ -10,6 +10,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
     <title>Paraíso En La Selva</title>
+    <link rel="icon" href="/view/img/Icon_logo.png" type="image/x-icon">
+
 </head>
 
 <script>
