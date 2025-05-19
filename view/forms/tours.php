d64bb59aa3fd62d6df4c21dd1c909ca5391a90ca
Lcho640 <horacio.moreno35@unach.mx>
2025-05-19 13:18:17 -0600

    se agrego boton a operadoras

--- view/forms/tours.php
+++ view/forms/tours.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es-MX">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,13 +9,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="view/css/style.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    
+
     <title>Tours Chiapas</title>
     <link rel="icon" href="/view/img/Icon_logo.png" type="image/x-icon">
 </head>
 
 <body>
-    <?php require_once("./view/layout/header.php");?>
+    <?php require_once("./view/layout/header.php"); ?>
     <br><br>
 
     <img src="./view/img/tour.png" class="img-fluid" alt="Tour Chiapas" width=100%>
@@ -22,75 +23,86 @@
     <br><br>
 
     <div class="container">
-        <p class="text-border">En estos tours, disfrutarás de una experiencia inolvidable con la agencia Chiapas Oculto. 
-            Te ofrecemos aventuras únicas llenas de emoción, exploración y momentos inolvidables. 
+        <p class="text-border">En estos tours, disfrutarás de una experiencia inolvidable con la agencia Chiapas Oculto.
+            Te ofrecemos aventuras únicas llenas de emoción, exploración y momentos inolvidables.
             ¡Prepárate para descubrir lo mejor del destino con nosotros!
         </p>
     </div>
 
     <br><br>
-    
-    <?php 
-                // Verificar si el usuario está logueado y tiene id_rango == 1
-                if (isset($_SESSION['id_rango']) && $_SESSION['id_rango'] == 1): ?>
-                 <center> <button type="submit" class="btn9">
-                 <p style="color: white;"><a href="index.php?u=btnagregarproducto">AGREGAR PRODUCTO</a></p>
-                 </button></center>                
-                 
+
+    <?php
+    // Verificar si el usuario está logueado y tiene id_rango == 1
+    if (isset($_SESSION['id_rango']) && $_SESSION['id_rango'] == 1): ?>
+        <center> <button type="submit" class="btn9">
+                <p style="color: white;"><a href="index.php?u=btnagregarproducto">AGREGAR PRODUCTO</a></p>
+            </button></center>
+
     <?php endif; ?>
 
-                    <?php 
-                // Verificar si el usuario está logueado y tiene id_rango == 1
-                if (isset($_SESSION['id_rango']) && $_SESSION['id_rango'] == 3): ?>
-                 <center> <button type="submit" class="btn9">
+    <?php
+    // Verificar si el usuario está logueado y tiene id_rango == 1
+    if (isset($_SESSION['id_rango']) && $_SESSION['id_rango'] == 3): ?>
+        <center> <button type="submit" class="btn9">
                 <p style="color: white;"><a href="index.php?u=btnagregarproducto"> AGREGAR PRODUCTO </a></p>
-              </button></center>     
-        <?php endif; ?>
+            </button></center>
+    <?php endif; ?>
 
 
     <center>
-    <table class="table paquetes">
-        <?php 
-        $counter = 0; // Contador para las filas
-        foreach ($paquetes as $paquete): 
-            if ($counter % 3 == 0) { 
-                echo '<tr>'; // Inicia una nueva fila cada 3 elementos
-            }
-        ?>
-            <td class="paquete-cell">
-                   <a href="<?php echo htmlspecialchars($paquete['urls']); ?>&id=<?php echo $paquete['id_catalogo'];?>">
-                    <img src="<?php echo $paquete['imagen']; ?>" alt="<?php echo htmlspecialchars($paquete['Nombre']); ?>" class="paquete-imagen">
-                    <h3 class="paquete-nombre"><?php echo htmlspecialchars($paquete['Nombre']); ?></h3>
-                    <p class="descripcion"><?php echo htmlspecialchars($paquete['Descripcion']); ?></p>
-                    <br>
-                    <p class="pequeno"><b>$<?php echo number_format($paquete['Precio'], 2); ?></b></p>
-                </a>
-                
-                <?php 
-                // Verificar si el usuario está logueado y tiene id_rango == 1
-                if (isset($_SESSION['id_rango']) && $_SESSION['id_rango'] == 1): ?>
-                    <div class="acciones">
-                    <a href="index.php?u=editar&id=<?php echo $paquete['id_catalogo'];?>" class="btn22"><b>EDITAR</b></a>
-                    <br>
-                        <a href="index.php?u=eliminardato&id=<?php echo $paquete['id_catalogo']; ?>" class="btn21" 
-                         onclick="return confirm('¿Estás seguro de que deseas eliminar este tour?');"><b>ELIMINAR</b></a>
+        <table class="table paquetes">
+            <?php
+            $counter = 0; // Contador para las filas
+            foreach ($paquetes as $paquete):
+                if ($counter % 3 == 0) {
+                    echo '<tr>'; // Inicia una nueva fila cada 3 elementos
+                }
+            ?>
+                <td class="paquete-cell">
+                    <a href="<?php echo htmlspecialchars($paquete['urls']); ?>&id=<?php echo $paquete['id_catalogo']; ?>">
+                        <img src="<?php echo $paquete['imagen']; ?>" alt="<?php echo htmlspecialchars($paquete['Nombre']); ?>" class="paquete-imagen">
+                        <h3 class="paquete-nombre"><?php echo htmlspecialchars($paquete['Nombre']); ?></h3>
+                        <p class="descripcion"><?php echo htmlspecialchars($paquete['Descripcion']); ?></p>
+                        <br>
+                        <p class="pequeno"><b>$<?php echo number_format($paquete['Precio'], 2); ?></b></p>
+                    </a>
+
+                    <?php
+                    // Verificar si el usuario está logueado y tiene id_rango == 1
+                    if (isset($_SESSION['id_rango']) && $_SESSION['id_rango'] == 1): ?>
+                        <div class="acciones">
+                            <a href="index.php?u=editar&id=<?php echo $paquete['id_catalogo']; ?>" class="btn22"><b>EDITAR</b></a>
+                            <br>
+                            <a href="index.php?u=eliminardato&id=<?php echo $paquete['id_catalogo']; ?>" class="btn21"
+                                onclick="return confirm('¿Estás seguro de que deseas eliminar este tour?');"><b>ELIMINAR</b></a>
                         </div>
-                <?php endif; ?>
-            </td>
-            <?php 
-            $counter++; 
-            if ($counter % 3 == 0) {
-                echo '</tr>'; // Cierra la fila después de 3 elementos
+                    <?php endif; ?>
+
+                   <?php
+                    // Verificar si el usuario está logueado y tiene id_rango == 1
+                    if (isset($_SESSION['id_rango']) && $_SESSION['id_rango'] == 3): ?>
+                        <div class="acciones">
+                            <a href="index.php?u=editar&id=<?php echo $paquete['id_catalogo']; ?>" class="btn22"><b>EDITAR</b></a>
+                            <br>
+                            <a href="index.php?u=eliminardato&id=<?php echo $paquete['id_catalogo']; ?>" class="btn21"
+                                onclick="return confirm('¿Estás seguro de que deseas eliminar este tour?');"><b>ELIMINAR</b></a>
+                        </div>
+                    <?php endif; ?>
+                </td>
+            <?php
+                $counter++;
+                if ($counter % 3 == 0) {
+                    echo '</tr>'; // Cierra la fila después de 3 elementos
+                }
+            endforeach;
+            // Si el número de paquetes no es múltiplo de 3, cierra la última fila
+            if ($counter % 3 != 0) {
+                echo '</tr>';
             }
-        endforeach;
-        // Si el número de paquetes no es múltiplo de 3, cierra la última fila
-        if ($counter % 3 != 0) {
-            echo '</tr>';
-        }
-        ?>
-    </table>
-</center>
-    
+            ?>
+        </table>
+    </center>
+
     <br><br><br><br><br><br><br>
 
     <table class="piepagina" width="100%">
@@ -101,23 +113,69 @@
                 <br><br>
             </td>
             <td width="22%">
-                <center><h5>Servicio al Cliente</h5></center>
-                <a href="index.php?u=contacto"><ul class="custom-bullets"><li>Contacto</li></ul></a>
-                <a href="index.php?u=login"><ul class="custom-bullets"><li>Mi Cuenta</li></ul></a>
+                <center>
+                    <h5>Servicio al Cliente</h5>
+                </center>
+                <a href="index.php?u=contacto">
+                    <ul class="custom-bullets">
+                        <li>Contacto</li>
+                    </ul>
+                </a>
+                <a href="index.php?u=login">
+                    <ul class="custom-bullets">
+                        <li>Mi Cuenta</li>
+                    </ul>
+                </a>
             </td>
             <td width="22%">
-                <center><h5>Tours Destacados</h5></center>
-                <a href="index.php?u=arcotete"><ul class="custom-bullets2"><li>El Arcote</li></ul></a>
-                <a href="index.php?u=lagunasmontebello"><ul class="custom-bullets2"><li>Lagunas de Montebello</li></ul></a>
-                <a href="index.php?u=palenque"><ul class="custom-bullets2"><li>Zona Arqueológica De Palenque</li></ul></a>
-                <a href="index.php?u=zonarqueologica"><ul class="custom-bullets2"><li>Zona Arqueológica De Tonina</li></ul></a>
+                <center>
+                    <h5>Tours Destacados</h5>
+                </center>
+                <a href="index.php?u=arcotete">
+                    <ul class="custom-bullets2">
+                        <li>El Arcote</li>
+                    </ul>
+                </a>
+                <a href="index.php?u=lagunasmontebello">
+                    <ul class="custom-bullets2">
+                        <li>Lagunas de Montebello</li>
+                    </ul>
+                </a>
+                <a href="index.php?u=palenque">
+                    <ul class="custom-bullets2">
+                        <li>Zona Arqueológica De Palenque</li>
+                    </ul>
+                </a>
+                <a href="index.php?u=zonarqueologica">
+                    <ul class="custom-bullets2">
+                        <li>Zona Arqueológica De Tonina</li>
+                    </ul>
+                </a>
             </td>
             <td width="22%">
-                <center><h5>Paquetes Destacados</h5></center>
-                <a href="index.php?u=arqueologiaselva"><ul class="custom-bullets2"><li>Arqueología y Selva</li></ul></a>
-                <a href="index.php?u=paraisoselva"><ul class="custom-bullets2"><li>Paraíso en la Selva</li></ul></a>
-                <a href="index.php?u=expedicionchiapaneca"><ul class="custom-bullets2"><li>Expedición Chiapaneca</li></ul></a>
-                <a href="index.php?u=rutasmchiapas"><ul class="custom-bullets2"><li>Rutas Mágicas de Chiapas</li></ul></a>
+                <center>
+                    <h5>Paquetes Destacados</h5>
+                </center>
+                <a href="index.php?u=arqueologiaselva">
+                    <ul class="custom-bullets2">
+                        <li>Arqueología y Selva</li>
+                    </ul>
+                </a>
+                <a href="index.php?u=paraisoselva">
+                    <ul class="custom-bullets2">
+                        <li>Paraíso en la Selva</li>
+                    </ul>
+                </a>
+                <a href="index.php?u=expedicionchiapaneca">
+                    <ul class="custom-bullets2">
+                        <li>Expedición Chiapaneca</li>
+                    </ul>
+                </a>
+                <a href="index.php?u=rutasmchiapas">
+                    <ul class="custom-bullets2">
+                        <li>Rutas Mágicas de Chiapas</li>
+                    </ul>
+                </a>
             </td>
             <td width="22%">
                 <br><br><br>
@@ -130,7 +188,8 @@
     </table>
 
     <br><br>
-    <?php require_once("./view/layout/footer.php");?>
-    
+    <?php require_once("./view/layout/footer.php"); ?>
+
 </body>
+
 </html>
